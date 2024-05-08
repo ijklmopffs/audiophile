0e0f5ad992d1607b4af16761fa432b0d687fd5d7
Nuel Elekwachi <elekwaeke300@gmail.com>
2024-05-08 06:10:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Audiophile E-commerce page
+
+This is an e-commerce page where you can buy musical products including headphones, speakers, and earphones.
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${manrope.variable} font-manrope`}>
+      <Component {...pageProps} />;
+    </main>
+  );
+}
